9b9c0c4ed1ac5ee420639c9531d37f6e00dafd49
Aaron-Larch <52671693+Aaron-Larch@users.noreply.github.com>
2020-11-25 08:50:30 -0500

    Bug fixes

--- src/main/java/com/webbuild/javabrains/repository/RoleRepository.java
+++ src/main/java/com/webbuild/javabrains/repository/RoleRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.webbuild.javabrains.model.Role;
 
 //Connect to The Role Table in the database
-public interface RoleRepository extends JpaRepository<Role, int>{
+public interface RoleRepository extends JpaRepository<Role, Integer>{
 }
