1109d7ae1436625183275085431d55ef3ba65e67
Aaron-Larch <52671693+Aaron-Larch@users.noreply.github.com>
2020-11-25 08:46:35 -0500

    Bug Fixes

--- src/main/java/com/webbuild/javabrains/repository/UserRepository.java
+++ src/main/java/com/webbuild/javabrains/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.webbuild.javabrains.model.User;
 
 //connect to the user table In the Database
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, int> {
     User findByUsername(String username);
     User findUserByEmail(String Email);
 }
