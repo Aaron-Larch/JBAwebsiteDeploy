d0ed39d62a6df71b9c9c5e60419db2a0996f52a0
Aaron-Larch <52671693+Aaron-Larch@users.noreply.github.com>
2020-11-25 10:01:19 -0500

    Bug fix

--- src/main/java/com/webbuild/javabrains/service/UserServiceImpl.java
+++ src/main/java/com/webbuild/javabrains/service/UserServiceImpl.java
@@ -46,6 +46,7 @@ public class UserServiceImpl implements UserService {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         user.setKeyanswer(bCryptPasswordEncoder.encode(user.getKeyanswer()));
         Security.add(roleRepository.findById(user.getRoleid()).get()); //call a single role that matches the user input 
+	    System.out.println(Security.toString());
         user.setRoles(Security);
         userRepository.save(user);
     }
