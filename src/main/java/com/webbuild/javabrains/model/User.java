0126718590391b1c8daa2b15fcdd5943921d464f
AaronLarch <Aaron.Larch@gce.org>
2020-12-02 12:40:34 -0500

    Bug fix

--- src/main/java/com/webbuild/javabrains/model/User.java
+++ src/main/java/com/webbuild/javabrains/model/User.java
@@ -27,12 +27,7 @@ public class User implements Serializable {
 	@Transient
     private String passwordConfirm;
 	
-	@ManyToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY) //Declare value as receiving value from other table
-	@JoinTable(
-			name = "users_DIVISIONS", //declare foren key name
-			joinColumns = @JoinColumn(name = "users_roleid", referencedColumnName = "roleid"), //declare users table
-			inverseJoinColumns = @JoinColumn(name = "role_DIVISIONID", referencedColumnName = "DIVISIONID")//declare divisions table 
-			) 
+	@ManyToMany //Declare value as receiving value from other table
     private Set<Role> DIVISIONS; //set a many to many relation with the Role table
     
     public int getId() {
